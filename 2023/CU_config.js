3ef92a5b8fb246d4c14ca5110f1820e36606800b
karweb2194 <124397688+karweb2194@users.noreply.github.com>
2023-03-15 19:22:29 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023ilpe",
+      "defaultValue": "2023ndgf",
       "required": "true",
       "disabled": "true"
     },
