6311cf7cc58c60e05eadc134967bc3b5cb70f18a
karweb2194 <124397688+karweb2194@users.noreply.github.com>
2023-03-18 21:19:03 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023ilpe",
+      "defaultValue": "2023wimi",
       "required": "true",
       "disabled": "true"
     },
